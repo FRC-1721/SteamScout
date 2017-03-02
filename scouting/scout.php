b648a89327b24735489e7ccd6435e11fcf07e8f9
brennan-macaig <brennanm@santbani.org>
2017-03-02 10:42:26 -0500

    Added scout.php base code.

---
+++ scouting/scout.php
@@ -0,0 +1,46 @@
+<?php
+
+
+  include "../base.php";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="SteamScout">
+    <meta name="author" content="Brennan Macaig, FRC-1721">
+    <title>SteamScout</title>
+    <!-- Bootstrap 4.0.0-alpha.6 core CSS -->
+    <link href="https://v4-alpha.getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="../css/font-awesome.min.css" rel="stylesheet"/>
+    <!-- Custom Style -->
+    <link href="../css/index.css">
+  </head>
+  <body>
+    <?php
+    if (!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username'])) {
+    ?>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle Navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href-"../scouting/">SteamScout</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active">Home</li>
+            <li><a href="../logout">Log Out</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
