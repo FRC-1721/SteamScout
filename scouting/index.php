3bf253c354d69fb32f4675cb19eb59896170b1e5
brennan-macaig <brennanm@santbani.org>
2017-03-14 14:44:52 -0400

    ooooops again

--- scouting/index.php
+++ scouting/index.php
@@ -56,7 +56,6 @@
     <?php
 
   } else if ($_GET['f']) {
-      case "1":
       ?>
       <div class="alert alert-danger alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
